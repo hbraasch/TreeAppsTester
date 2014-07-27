b5b33e295c6c5793a7760ce1b09047431b2f0606
hrbraasch@gmail.com <hrbraasch@gmail.com>
2014-07-27 10:57:45 +1000

    Added

---
+++ TreeNotesTester/Models/TreeNotesController.vb
@@ -0,0 +1,11 @@
+﻿Public Class TreeNotesController
+    Inherits System.Web.Mvc.Controller
+
+    '
+    ' GET: /TreeNotes
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+End Class
